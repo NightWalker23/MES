4db4d832b149f5e1d95bcc66134cb64a0469ee05
NightWalker23 <taptnw@gmail.com>
2018-01-04 13:03:36 +0100

    poprawki

--- calka/src/Calka_podwojna.java
+++ calka/src/Calka_podwojna.java
@@ -6,8 +6,12 @@ public class Calka_podwojna {
     private double[] tab3p = { -0.7745, 0, 0.7745 };
     private double[] w3p = { 5.0/9.0, 8.0/9.0, 5.0/9.0 };
 
+    private double[] tab5p = { -0.9, -0.53, 0, 0.53, 0.9 };
+    private double[] w5p = { 0.24, 0.48, 0.56, 0.48, 0.24 };
+
     private double funkcja ( double x, double y ) {
-        return ( 2 * Math.pow( x, 2 ) * Math.pow( y, 2 ) + 6 * x + 5 );
+        //return ( 2 * Math.pow( x, 2 ) * Math.pow( y, 2 ) + 6 * x + 5 );
+        return (3*x*x + 3*x + 1);
     }
 
     public double oblicz_2p (  ) {
@@ -26,6 +30,17 @@ public class Calka_podwojna {
         for ( int i = 0; i < 3; i ++ )
             for ( int j = 0; j < 3; j ++ )
                 suma += funkcja( tab3p[i], tab3p[j] ) * w3p[i] * w3p[j];
+
+        return suma;
+    }
+
+
+    public double oblicz_5p (  ) {
+        double suma = 0.0;
+
+        for ( int i = 0; i < 5; i ++ )
+            for ( int j = 0; j < 5; j ++ )
+                suma += funkcja( tab5p[i], tab5p[j] ) * w5p[i] * w5p[j];
 
         return suma;
     }
--- calka/src/Main.java
+++ calka/src/Main.java
@@ -5,8 +5,10 @@ public class Main {
 
         double wynik1 = calka.oblicz_2p();
         double wynik2 = calka.oblicz_3p();
+        double wynik5 = calka.oblicz_5p();
 
-        System.out.println( "Wynik 2p = " + wynik1 );
-        System.out.println( "Wynik 3p = " + wynik2 );
+        //System.out.println( "Wynik 2p = " + wynik1 );
+        //System.out.println( "Wynik 3p = " + wynik2 );
+        System.out.println( "Wynik 5p = " + wynik5 );
     }
 }
